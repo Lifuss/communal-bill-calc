cd6f7d049588758957793d53fea66e00b30e7a79
Arsen Hryhoriak <arsen.hryh@gmail.com>
2025-03-15 13:41:28 +0200

    next-auth type fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -16,13 +16,11 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // 1. Дістаємо дані з credentials
         const { email, password } = credentials as {
           email: string;
           password: string;
         };
 
-        // 2. Знаходимо користувача в БД
         const user = await prisma.user.findUnique({
           where: { email },
         });
@@ -31,40 +29,40 @@ const handler = NextAuth({
           throw new Error("No user found");
         }
 
-        // 3. Перевіряємо пароль
         const isValid = await bcrypt.compare(password, user.password);
         if (!isValid) {
           throw new Error("Password is incorrect");
         }
 
-        // 4. Якщо все ок, повертаємо об'єкт user
         return {
           id: user.id,
-          name: user.name, // або user.fullName
+          name: user.name,
           email: user.email,
         };
       },
     }),
   ],
   session: {
-    strategy: "jwt", // За бажанням, session-based чи jwt-based
+    strategy: "jwt",
   },
   callbacks: {
     async jwt({ token, user }) {
-      // user буде доступним лише у момент логіну
       if (user) {
-        token.id = user.id; // Запишемо id у токен
+        token.id = user.id;
       }
       return token;
     },
     async session({ session, token }) {
-      // Запишемо у session.id те, що є у токені
-      if (token) {
-        session.user.id = token.id as string;
+      if (token && token.id) {
+        if (session.user) {
+          session.user.id = token.id as string;
+        }
       }
       return session;
     },
   },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
