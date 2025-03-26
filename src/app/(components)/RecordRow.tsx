3eeb2c8214b7cb0cdc091bd4cd7d6fc4cbea4c86
Arsen Hryhoriak <arsen.hryh@gmail.com>
2025-03-26 20:21:09 +0200

    recordsTable

--- src/app/(components)/RecordRow.tsx
+++ src/app/(components)/RecordRow.tsx
@@ -1,41 +1,71 @@
-import { Records } from "@/lib/types/sharedTypes";
-import Image from "next/image";
+import { Records, RecordType } from "@/lib/types/sharedTypes";
 import React from "react";
 
-const RecordRow = ({ record }: { record: Records }) => {
+const RecordRow = ({
+  record,
+  tableType = "electricity",
+}: {
+  record: Records;
+  tableType: RecordType;
+}) => {
   const { createdAt, id, type, userId, nightPrice, nightUsage, price, usage } =
     record;
 
+  if (tableType === "gas") {
+    return (
+      <tr
+        key={id}
+        className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
+      >
+        <td className="px-6 py-4">
+          {new Date(createdAt).toLocaleDateString("uk-UA")}
+        </td>
+        <td className="px-6 py-4">{usage}</td>
+        <td className="px-6 py-4">Заглушка</td>
+        <td className="px-6 py-4">{price}</td>
+        <td className="px-6 py-4">
+          {typeof usage === "number" &&
+            typeof price === "number" &&
+            (usage * price).toFixed(2)}
+        </td>
+        <td className="px-6 py-4">
+          <a
+            href="#"
+            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            Ред.
+          </a>
+        </td>
+      </tr>
+    );
+  }
   return (
     <tr
       key={id}
       className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
     >
-      <th
-        scope="row"
-        className="px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex"
-      >
-        <Image
-          src={`/assets/${type}.png`}
-          alt={`знак ${type === "electricity" ? "електроенергії" : "газу"}`}
-          width={24}
-          height={24}
-        />{" "}
-        <p>{type === "electricity" ? "Електроенергія" : "Газ"}</p>
-      </th>
       <td className="px-6 py-4">
         {new Date(createdAt).toLocaleDateString("uk-UA")}
       </td>
       <td className="px-6 py-4">{usage}</td>
-      <td className="px-6 py-4">Заглушка</td>
-      <td className="px-6 py-4">{}</td>
+      <td className="px-6 py-4">Динаміка --</td>
+      <td className="px-6 py-4">{nightUsage}</td>
+      <td className="px-6 py-4">Динаміка ніч</td>
       <td className="px-6 py-4">{price}</td>
+      <td className="px-6 py-4">{nightPrice}</td>
+      <td className="px-6 py-4">
+        {typeof usage === "number" &&
+          typeof price === "number" &&
+          typeof nightUsage === "number" &&
+          typeof nightPrice === "number" &&
+          (usage * price + nightUsage * nightPrice).toFixed(2)}
+      </td>
       <td className="px-6 py-4">
         <a
           href="#"
           className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
         >
-          Edit
+          Ред.
         </a>
       </td>
     </tr>
--- src/app/(components)/RecordsList.tsx
+++ src/app/(components)/RecordsList.tsx
@@ -4,14 +4,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import SkeletonList from "./skeletons/SkeletonList";
-import { Records } from "@/lib/types/sharedTypes";
-import Image from "next/image";
+import { Records, RecordType } from "@/lib/types/sharedTypes";
 import RecordRow from "./RecordRow";
+import Image from "next/image";
 
 const fetchRecords = async (): Promise<any[]> => {
   const res = await fetch("/api/records");
   if (!res.ok) {
-    // Тут можна розпарсити помилку з відповіді, якщо потрібно
     console.log(res);
 
     throw new Error("Failed to fetch records");
@@ -19,10 +18,27 @@ const fetchRecords = async (): Promise<any[]> => {
   return res.json();
 };
 
-const RecordsList = () => {
+const RecordsList = ({
+  tableType = "electricity",
+}: {
+  tableType: RecordType;
+}) => {
   const { data: session, status } = useSession();
 
-  // Виконуємо запит лише коли користувач автентифікований
+  if (status === "unauthenticated") {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Неавторизовано</h1>
+        <p className="text-xl">
+          <Link href="/auth/login" className="underline">
+            Ввійдіть
+          </Link>{" "}
+          в аккаунт для продовження
+        </p>
+      </div>
+    );
+  }
+
   const { data, error, isLoading, refetch } = useQuery({
     queryKey: ["records"],
     queryFn: fetchRecords,
@@ -32,61 +48,110 @@ const RecordsList = () => {
   if (status === "loading" || isLoading) {
     return <SkeletonList />;
   }
-
-  if (status === "unauthenticated") {
-    return (
-      <div>
-        <h1 classNameName="text-2xl font-bold">Неавторизовано</h1>
-        <p classNameName="text-xl">
-          <Link href="/auth/login" classNameName="underline">
-            Ввійдіть
-          </Link>{" "}
-          в аккаунт для продовження
-        </p>
-      </div>
-    );
-  }
-
   if (error instanceof Error) {
     return <div>Error: {error.message}</div>;
+  }
+  let filteredData: Records[] = [];
+
+  if (tableType === "electricity" && data) {
+    filteredData = data.filter(
+      (record: Records) => record.type === "electricity"
+    );
+  } else if (tableType === "gas" && data) {
+    filteredData = data.filter((record: Records) => record.type === "gas");
   }
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="flex justify-center">
+        <Image
+          src={`/assets/${tableType}.png`}
+          alt={`знак ${
+            tableType === "electricity" ? "електроенергії" : "газу"
+          }`}
+          width={24}
+          height={24}
+        />
+        <h2 className="text-xl">
+          {tableType === "electricity" ? "Електроенергія" : "Газ"}
+        </h2>
+      </div>
+
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Вид
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Дата
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Використано (Кв/г|Куб)
-            </th>
-            <th
-              title="Різниця відносно останнього запису"
-              scope="col"
-              className="px-6 py-3"
-            >
-              Динаміка
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Ціна\од
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Сума
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Дія
-            </th>
-          </tr>
+          {tableType === "gas" ? (
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Дата
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Використано(Куб)
+              </th>
+              <th
+                title="Різниця відносно останнього запису"
+                scope="col"
+                className="px-6 py-3"
+              >
+                Динаміка
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Ціна\од
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Сума
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Дія
+              </th>
+            </tr>
+          ) : (
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Дата
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Використано(Кв/г)
+              </th>
+              <th
+                title="Різниця відносно останнього запису"
+                scope="col"
+                className="px-6 py-3"
+              >
+                Динаміка
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Використано Ніч.(Кв/г)
+              </th>
+              <th
+                title="Різниця відносно останнього запису"
+                scope="col"
+                className="px-6 py-3"
+              >
+                Динаміка Ніч.
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Тариф(грн)
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Тариф Ніч.(грн)
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Сума
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Дія
+              </th>
+            </tr>
+          )}
         </thead>
         <tbody>
-          {data && data.length > 0
-            ? data.map((record: Records) => (
-                <RecordRow key={record.id} record={record} />
+          {filteredData && filteredData.length > 0
+            ? filteredData.map((record: Records) => (
+                <RecordRow
+                  key={record.id}
+                  record={record}
+                  tableType={tableType}
+                />
               ))
             : "Даних нема"}
         </tbody>
